0d19f4c934d241b4e5c1f47da6ddfcca3a5f5ab2
SaschaG <ffSaschaGff@yandex.ru>
2018-10-03 12:12:10 +0300

    Созранение настроек

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,36 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.util.HashMap;
+
 public class Controller {
+
+    private static final String[] settingsFields = {"SQLServerField","SQLBDField","SQLUserField","SQLPasswordField"};
+
+    @FXML
+    private Button SaveSettingsButton;
+
+    public void saveSettings(ActionEvent actionEvent) {
+        HashMap<String, String> settings = new HashMap<String, String>();
+        for(String settingsField: Controller.settingsFields) {
+            TextField currentField = (TextField) ((Button) actionEvent.getSource()).getScene().lookup("#"+settingsField);
+            settings.put(settingsField,currentField.getText());
+        }
+        SettingsKeepper.saveSettings(settings);
+    }
+
+    public void propertyTabSelected(Event event) {
+        HashMap<String, String> settings = SettingsKeepper.loadSettings();
+        for(String settingsName: Controller.settingsFields) {
+            if(settings.containsKey(settingsName)) {
+                TextField currentField = (TextField) (SaveSettingsButton.getScene().lookup("#"+settingsName));
+                currentField.setText(settings.get(settingsName));
+            }
+        }
+    }
 }
---
+++ src/sample/SettingsKeepper.java
@@ -0,0 +1,47 @@
+package sample;
+
+import com.sun.scenario.Settings;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Properties;
+
+public class SettingsKeepper {
+
+    private static final String HATH_TO_PROPERTIES = "src/sample/resources/config.properties";
+
+    public static void saveSettings(HashMap<String, String> settingsMap) {
+        Properties properties = new Properties();
+        for(HashMap.Entry<String, String> entry: settingsMap.entrySet()) {
+            properties.put(entry.getKey(), entry.getValue());
+        }
+        try {
+            FileOutputStream fos = new FileOutputStream(SettingsKeepper.HATH_TO_PROPERTIES);
+            properties.store(fos, "");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static HashMap<String, String> loadSettings() {
+        HashMap<String, String> settingsMap = new HashMap<String, String>();
+        try {
+            FileInputStream fis = new FileInputStream(SettingsKeepper.HATH_TO_PROPERTIES);
+            Properties properties = new Properties();
+            properties.load(fis);
+            for(String propKey: properties.stringPropertyNames()){
+                settingsMap.put(propKey, properties.getProperty(propKey));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return settingsMap;
+    }
+}
